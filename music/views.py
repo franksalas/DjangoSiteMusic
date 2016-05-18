b5c0e64e5e4b5296235cce0748144ce09e284668
Frank Salas <Frank.salas@gmail.com>
2016-05-18 18:19:16 -0500

    End of Django Tutorial for Beginners  31 ModelForm and CreateView

--- music/views.py
+++ music/views.py
@@ -1,5 +1,6 @@
 from django.views import generic
 from .models import Album
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 class IndexView(generic.ListView):
     template_name = 'music/index.html'
@@ -12,3 +13,11 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Album
     template_name = 'music/detail.html'
+
+
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields = [
+        'artist', 'album_title', 'genre', 'album_logo'
+    ]
