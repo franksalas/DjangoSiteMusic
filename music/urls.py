a0646b88c5914a183fbf22aeebddae0919fd8568
Frank Salas <Frank.salas@gmail.com>
2016-05-18 13:53:39 -0500

    End of Django Tutorial for Beginners  12 - Writing Another View

--- music/urls.py
+++ music/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
+    # /music/
     url(r'^$', views.index, name='index'),
+
+    # /music/712
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("This is the music app homepage!")
+
+
+def detail(request, album_id):
+    return HttpResponse('<h2>  Details for album id </h2>')
