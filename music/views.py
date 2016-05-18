563356573e63d32fbb13bd869be9c842cd33e81f
Frank Salas <Frank.salas@gmail.com>
2016-05-18 14:17:09 -0500

    End of Django Tutorial for Beginners  14 - Templates

--- music/views.py
+++ music/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from .models import Album
+
 
 def index(request):
-    return HttpResponse("This is the music app homepage!")
+    all_albums = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context, request ))
 
 
 def detail(request, album_id):
