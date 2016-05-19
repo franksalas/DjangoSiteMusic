527c816f071e67d227008ebde13fb51a29ae61a5
Frank Salas <Frank.salas@gmail.com>
2016-05-18 20:10:10 -0500

    views and details format is still wrong but functional End of Django Tutorial for Beginners  33 upload files

--- music/views.py
+++ music/views.py
@@ -1,6 +1,7 @@
 from django.views import generic
 from .models import Album
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 
 class IndexView(generic.ListView):
     template_name = 'music/index.html'
@@ -15,9 +16,16 @@ class DetailView(generic.DetailView):
     template_name = 'music/detail.html'
 
 
-
 class AlbumCreate(CreateView):
     model = Album
-    fields = [
-        'artist', 'album_title', 'genre', 'album_logo'
-    ]
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
